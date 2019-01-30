af35dc74993da4a12884760cc8a070c9378fbcbc
kuruspepe <pepeka72@gmail.com>
2019-01-30 19:29:10 +0100

    traduzione e lflash

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -28,7 +28,7 @@ class ContentsController < ApplicationController
 
     respond_to do |format|
       if @content.save
-        format.html { redirect_to @content, notice: 'Content was successfully created.' }
+        format.html { redirect_to @content, notice: 'Contenuto creato con successo.' }
       else
         format.html { render :new }
       end
@@ -39,7 +39,7 @@ class ContentsController < ApplicationController
   def update
     respond_to do |format|
       if @content.update(content_params)
-        format.html { redirect_to @content, notice: 'Content was successfully updated.' }
+        format.html { redirect_to @content, notice: 'Contenuto aggiornato.' }
       else
         format.html { render :edit }
       end
@@ -51,7 +51,7 @@ class ContentsController < ApplicationController
   def destroy
     @content.destroy
     respond_to do |format|
-      format.html { redirect_to contents_url, notice: 'Content was successfully destroyed.' }
+      format.html { redirect_to contents_url, notice: 'Contenuto cancellato.' }
     end
   end
 
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module LacerMarket
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.i18n.default_locale = :it
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
